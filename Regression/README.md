63b218e6aa260b247c3ede72e9e18c00d4a73de3
Song Xu <song.xu.ucla@gmail.com>
2019-03-04 19:48:33 -0800

    Residual plot with seaborn

---
+++ Regression/README.md
@@ -0,0 +1,15 @@
+
+
+Regression might be considered "too easy/trivial", but is really important.
+
+It is the most basic way to model global patterns. On the contrary, NN models the local pattern. 
+(insights from Elements of Statistical Learning)
+
+
+Variations of linear regression by introducing bias (a lot of jargon!):
++ L1 Norm (Lasso)
++ L2 Norm (Ridge)
++ both Norms (Elastic Net)
+
+Diagnostic of linear regression:
+1. Residuals vs. Fitted: Shows if residuals 
---
+++ Regression/main_plots.py
@@ -0,0 +1,29 @@
+
+
+import numpy as np
+import pandas as pd
+import os
+from sklearn import linear_model
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+data_folderpath = 'house_data'
+
+train_df = pd.read_csv(os.path.join(data_folderpath, 'kc_house_train_data.csv'))
+print train_df.head()
+
+X=train_df['sqft_living'].values
+y=train_df['price'].values
+
+# regr = linear_model.LinearRegression()
+# regr.fit(X, y)
+# print regr.intercept_, regr.coef_
+
+print X.shape
+print y.shape
+sns.residplot(X, y, lowess=True)
+plt.show()
