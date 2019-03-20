18c22c8568ba0d1711962d3692d95fc7df0389a3
Song Xu <song.xu.ucla@gmail.com>
2019-03-20 09:43:13 -0700

    Handle test data (0,4) and (4,12)

--- Recommender_Systems/ClinicalOrders_LSTM/data_preparation.py
+++ Recommender_Systems/ClinicalOrders_LSTM/data_preparation.py
@@ -5,9 +5,26 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('data/patient_items_50000_sample.csv')
 print df.shape
 
-all_pats = df['patient_id'].drop_duplicates().values
 
-pats_train, pats_test = train_test_split(all_pats)
+
+'''
+Remapping patient and item ids
+'''
+old_pat_ids = df['patient_id'].drop_duplicates().values.tolist()
+num_unique_pats = len(old_pat_ids)
+new_pats_ids = range(1,num_unique_pats+1)
+pat_mapping = dict(zip(old_pat_ids, new_pats_ids))
+df['patient_id_old'] = df['patient_id']
+df['patient_id'] = df['patient_id'].apply(lambda x: pat_mapping[x])
+
+old_item_ids = df['clinical_item_id'].drop_duplicates().values.tolist()
+num_unique_items = len(old_item_ids)
+new_item_ids = range(1,num_unique_items+1)
+item_mapping = dict(zip(old_item_ids, new_item_ids))
+df['clinical_item_id_old'] = df['clinical_item_id']
+df['clinical_item_id'] = df['clinical_item_id'].apply(lambda x: item_mapping[x])
+
+pats_train, pats_test = train_test_split(new_pats_ids)
 
 df_train = df[df['patient_id'].isin(pats_train)]
 df_test = df[df['patient_id'].isin(pats_test)]
--- Recommender_Systems/ClinicalOrders_LSTM/main_spotlight.py
+++ Recommender_Systems/ClinicalOrders_LSTM/main_spotlight.py
@@ -1,66 +1,79 @@
 
 import pandas as pd
+pd.set_option('display.width', 10000)
 import numpy as np
 
 from spotlight.interactions import Interactions
 from spotlight.sequence.implicit import ImplicitSequenceModel
 
-df_train = pd.read_csv('data/patient_items_50000_sample_train.csv')
-print 'df_train.shape:', df_train.shape
+# df_train = pd.read_csv('data/patient_items_50000_sample_train.csv')\
+#     .sort_values(['patient_id', 'item_date'])
+# print 'df_train.shape:', df_train.shape
+#
+# pat_ids_train = df_train['patient_id'].drop_duplicates().values.tolist()
+# print 'number of train patients: ', len(pat_ids_train)
+#
+# '''
+# I'm really amazed by this pandas trick, tho it is not useful:
+#
+# grouped_pat_items = df.groupby('patient_id')['clinical_item_id'].apply(list).reset_index()
+# '''
+#
+#
+# '''
+# Prepare training data
+# '''
+# users_train = df_train['patient_id'].values
+# items_train = df_train['clinical_item_id'].values
+# times_train = df_train['item_date'].values
+#
+#
+# '''
+# Feed training data into model for training
+# '''
+# implicit_interactions = Interactions(users_train,
+#                                      items_train,
+#                                      timestamps=times_train)
+# sequential_interaction = implicit_interactions.to_sequence()
+# implicit_sequence_model = ImplicitSequenceModel()
+# implicit_sequence_model.fit(sequential_interaction)
 
-pat_ids_train = df_train['patient_id'].drop_duplicates().values.tolist()
-print 'number of train patients: ', len(pat_ids_train)
-
-
-'''
-Remapping patient and item ids
-'''
-old_pat_ids = df_train['patient_id'].drop_duplicates().values.tolist()
-num_unique_pats = len(old_pat_ids)
-pat_mapping = dict(zip(old_pat_ids, range(1,num_unique_pats+1)))
-df_train['patient_id'] = df_train['patient_id'].apply(lambda x: pat_mapping[x])
-
-old_item_ids = df_train['clinical_item_id'].drop_duplicates().values.tolist()
-num_unique_items = len(old_item_ids)
-item_mapping = dict(zip(old_item_ids, range(1,num_unique_items+1)))
-df_train['clinical_item_id'] = df_train['clinical_item_id'].apply(lambda x: item_mapping[x])
-
-
-'''
-I'm really amazed by this pandas trick, tho it is not useful:
-
-grouped_pat_items = df.groupby('patient_id')['clinical_item_id'].apply(list).reset_index()
-'''
-
-
-'''
-Prepare training data
-'''
-users_train = df_train['patient_id'].values
-items_train = df_train['clinical_item_id'].values
-times_train = df_train['item_date'].values
-
-
-'''
-Feed training data into model for training
-'''
-implicit_interactions = Interactions(users_train,
-                                     items_train,
-                                     timestamps=times_train)
-sequential_interaction = implicit_interactions.to_sequence()
-implicit_sequence_model = ImplicitSequenceModel()
-implicit_sequence_model.fit(sequential_interaction)
 
 
 '''
 Testing
 '''
-df_test = pd.read_csv('data/patient_items_50000_sample_test.csv')
+df_test = pd.read_csv('data/patient_items_50000_sample_test.csv')\
+    .sort_values(['patient_id', 'item_date'])
 print 'df_test.shape:', df_test.shape
-df_test = df_test.sort_values(['patient_id', 'item_date'])
 
 pat_ids_test = df_test['patient_id'].drop_duplicates().values.tolist()
-print 'number of train patients: ', len(pat_ids_test)
+print 'number of test patients: ', len(pat_ids_test)
+
+'''
+Check: for each patient, how many orders fall into 0-4 hrs? 
+How many fall into 4-12 hrs?
+'''
+
+from datetime import datetime, timedelta
+for pat_id in pat_ids_test:
+    df_cur = df_test[df_test['patient_id']==pat_id].copy().reset_index()
+
+    df_cur['item_date'] = df_cur['item_date'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+    )
+    first_date = df_cur.ix[0, 'item_date']
+        # df_cur[['patient_id','item_date']].groupby('patient_id')\
+        #             .first().values[0]
+    df_cur['seconds_rela'] = df_cur['item_date'].apply(
+        lambda x: (x-first_date).total_seconds()
+    )
+
+    within_4_hours = df_cur[df_cur['seconds_rela'] < 4*3600]
+    between_4_and_12_hours = df_cur[(4 * 3600 < df_cur['seconds_rela'])
+                                        & (df_cur['seconds_rela'] < 12 * 3600)]
+    print len(within_4_hours), len(between_4_and_12_hours)
+
 
 def get_top_k(scores, k=10):
     return [x+1 for x in np.argsort(scores)[-k:]]
@@ -80,10 +93,10 @@ for pat_id in pat_ids_test:
     Needs optimization!
     '''
     query_set = [cur_orders[0]]
-    query_set = [item_mapping.get(x,0) for x in query_set]
+    # query_set = [item_mapping.get(x,0) for x in query_set]
 
     valid_set = cur_orders[1:]
-    valid_set = [item_mapping.get(x,0) for x in valid_set]
+    # valid_set = [item_mapping.get(x,0) for x in valid_set]
 
     all_recommends = implicit_sequence_model.predict(query_set)
     top_k_recommends = get_top_k(all_recommends, k=10)
