38877e43d77ac4040d2834d3e598e58e93b0e3a7
Song Xu <song.xu.ucla@gmail.com>
2019-10-30 22:26:19 -0700

    udemy spark

---
+++ Spark/ratings-counter.py
@@ -0,0 +1,11 @@
+
+from pyspark import SparkConf, SparkContext
+
+
+conf = SparkConf().setMaster("local").setAppName("RatingHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+print result.items()
