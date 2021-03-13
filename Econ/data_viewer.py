df8c3fe1f0d90c4269c15a87ef2ef6f4d949008b
sxu11 <song.xu.ucla@gmail.com>
2021-03-12 21:33:12 -0800

    align all data

---
+++ Econ/data_viewer.py
@@ -0,0 +1,78 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+sp_data = pd.read_csv("manual_data/SP500.csv")
+"""2011-03-14 to 2021-03-12"""
+
+cpi_data = pd.read_csv("manual_data/CPALTT01USM657N.csv")
+m1_data = pd.read_csv("manual_data/WM1NS.csv")
+baa_data = pd.read_csv("manual_data/BAA10Y.csv")
+gdp_data = pd.read_csv("manual_data/GDP.csv")
+
+# print(sp_data.shape[0], cpi_data.shape[0], m1_data.shape[0]*7, baa_data.shape[0]*1, gdp_data.shape[0]*90)
+
+print(sp_data.head(3))
+print(cpi_data.head(3))
+print(m1_data.head(3))
+print(baa_data.head(3))
+print(gdp_data.head(3))
+
+idx = pd.date_range("2016-03-14", "2021-01-01")
+
+for df in [sp_data, cpi_data, m1_data, baa_data, gdp_data]:
+    df["DATE"] = pd.to_datetime(df["DATE"], format='%Y-%m-%d')
+
+firstVals = []
+"""
+what is the latest available value before "2016-03-14"?
+"""
+for df in [sp_data, cpi_data, m1_data, baa_data, gdp_data]:
+    prev_val = None
+    for i in range(df.shape[0]):
+        dateVal = df.iloc[i].values
+        if dateVal[0] > idx[0]:
+            firstVals.append(float(prev_val))
+            break
+        prev_val = dateVal[1]
+print(firstVals)
+
+sp_series = sp_data.set_index("DATE").squeeze().reindex(idx)
+cpi_series = cpi_data.set_index("DATE").squeeze().reindex(idx)
+m1_series = m1_data.set_index("DATE").squeeze().reindex(idx)
+baa_series = baa_data.set_index("DATE").squeeze().reindex(idx)
+gdp_series = gdp_data.set_index("DATE").squeeze().reindex(idx)
+
+print("After squeeze:")
+
+print(sp_series.head(5))
+print(cpi_series.head(5))
+print(m1_series.head(5))
+print(baa_series.head(5))
+print(gdp_series.head(5))
+
+def fillInHistoryValue(ds, firstVal):
+    curVal = firstVal
+    for i in range(ds.shape[0]):
+        if ds.iloc[i] != ds.iloc[i]:
+            ds.iloc[i] = curVal
+        else:
+            curVal = ds.iloc[i]
+    return ds
+
+for i, ds in enumerate([sp_series, cpi_series, m1_series, baa_series, gdp_series]):
+    # df["DATE"] = pd.to_datetime(df["DATE"], format='%Y-%m-%d')
+    fillInHistoryValue(ds, firstVals[i])
+
+print("After fillInHistoryValue:")
+
+print(sp_series.head(5))
+print(cpi_series.head(5))
+print(m1_series.head(5))
+print(baa_series.head(5))
+print(gdp_series.head(5))
+"""data seems only 7 year"""
+# plt.plot(sp_data["0"])
+# plt.ylim([0,3500])
+# plt.show()
+
--- Econ/test_fred.py
+++ Econ/test_fred.py
@@ -1,14 +1,53 @@
 
 """
 https://pypi.org/project/fredapi/
+
+https://seekingalpha.com/article/4395421-modeling-fair-value-of-s-and-p-500
+
+https://mortada.net/python-api-for-fred.html
+
+https://fred.stlouisfed.org/searchresults/?st=CPI
 """
 
 import pandas as pd
 from fredapi import Fred
 fred = Fred(api_key="39046de0f8665857c262677588e22e46")
 
-s = fred.get_series('SP500', observation_start='2014-09-02', observation_end='2014-09-05')
-print(s.tail())
+observation_start='1960-01-01'
+observation_end='2021-01-01'
 
-g = fred.get_series('GDP', observation_start='2014-03-02', observation_end='2014-09-05')
+"""
+daily
+"""
+s = fred.get_series('SP500', observation_start=observation_start, observation_end=observation_end)
+print(s.tail())
+s.to_csv("data/sp500_19600101_2021_0101.csv", index=False)
+
+
+"""
+3-month
+"""
+g = fred.get_series('GDP', observation_start=observation_start, observation_end=observation_end)
 print(g.tail())
+g.to_csv("data/gdp_19600101_2021_0101.csv", index=False)
+
+"""
+weekly
+"""
+g = fred.get_series('M1', observation_start=observation_start, observation_end=observation_end)
+print(g.tail())
+g.to_csv("data/m1_19600101_2021_0101.csv", index=False)
+
+"""
+daily
+"""
+g = fred.get_series('BAA10Y', observation_start=observation_start, observation_end=observation_end)
+print(g.tail())
+g.to_csv("data/baa10y_19600101_2021_0101.csv", index=False)
+
+"""
+monthly
+"""
+g = fred.get_series('CPALTT01USM657N', observation_start=observation_start, observation_end=observation_end)
+print(g.tail())
+g.to_csv("data/cpi_19600101_2021_0101.csv", index=False)
